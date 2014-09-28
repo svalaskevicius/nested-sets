1b219b62265b7d73c2352e3f45b1b3267c7cab64
Sarunas Valaskevicius <rakatan@gmail.com>
2014-09-28 21:28:41 +0100

    introduce position in nested sets

--- src/lib/Data/NestedSet.hs
+++ src/lib/Data/NestedSet.hs
@@ -1,17 +1,19 @@
 module Data.NestedSet (
     NestedSets,
+    Position,
     NestedSetsNode(..),
     forestToNestedSets,
     nestedSetsToForest,
+    nestedSetsStartPosition,
     ) where
 
 import Data.Tree (Forest, subForest, rootLabel, Tree(..))
 
 type NestedSets a = [NestedSetsNode a]
+type Position = (Int, Int)
 
 data NestedSetsNode a = NestedSetsNode {
-    left :: Int,
-    right :: Int,
+    position :: Position,
     content :: a,
     children :: NestedSets a
 } deriving (Show, Eq)
@@ -25,8 +27,11 @@ forestToNestedSets = fst . nestedSetsStartingAt ([], 0)
                 (subForestNestedSets, end) = nestedSetsStartingAt ([], currentElementStart) $ subForest el
                 currentElementEnd = end + 1
                 elementContent = rootLabel el
-            in (siblingCapacities ++ [NestedSetsNode currentElementStart currentElementEnd elementContent subForestNestedSets], currentElementEnd)
+            in (siblingCapacities ++ [NestedSetsNode (currentElementStart, currentElementEnd) elementContent subForestNestedSets], currentElementEnd)
 
 nestedSetsToForest :: NestedSets a -> Forest a
 nestedSetsToForest = map (\el -> Node (content el) (nestedSetsToForest $ children el))
 
+nestedSetsStartPosition :: NestedSets a -> Maybe Position
+nestedSetsStartPosition [] = Nothing
+nestedSetsStartPosition (first:_) = Just . position $ first
--- test/Data/NestedSetSpec.hs
+++ test/Data/NestedSetSpec.hs
@@ -14,13 +14,13 @@ spec = describe "Nested set" $ do
             forestToNestedSets [] `shouldBe` ([]::NestedSets Char)
 
         it "generates nested sets for one node" $ do
-            forestToNestedSets [Node 'a' []] `shouldBe` [NestedSetsNode 1 2 'a' []]
+            forestToNestedSets [Node 'a' []] `shouldBe` [NestedSetsNode (1, 2) 'a' []]
 
         it "generates nested sets for two nodes" $ do
-            forestToNestedSets [Node 'a' [], Node 'b' []] `shouldBe` [NestedSetsNode 1 2 'a' [], NestedSetsNode 3 4 'b' []]
+            forestToNestedSets [Node 'a' [], Node 'b' []] `shouldBe` [NestedSetsNode (1, 2) 'a' [], NestedSetsNode (3, 4) 'b' []]
 
         it "generates nested sets for nested nodes" $ do
-            forestToNestedSets [Node 'a' [Node 'b' []]] `shouldBe` [NestedSetsNode 1 4 'a' [NestedSetsNode 2 3 'b' []]]
+            forestToNestedSets [Node 'a' [Node 'b' []]] `shouldBe` [NestedSetsNode (1, 4) 'a' [NestedSetsNode (2, 3) 'b' []]]
 
         it "generates nested sets for several nested nodes" $ do
             forestToNestedSets complexForest `shouldBe` complexNestedSets
@@ -30,16 +30,22 @@ spec = describe "Nested set" $ do
             nestedSetsToForest [] `shouldBe` ([]::Forest Char)
 
         it "generates forest for one node" $ do
-            nestedSetsToForest [NestedSetsNode 1 2 'a' []] `shouldBe` [Node 'a' []]
+            nestedSetsToForest [NestedSetsNode (1, 2) 'a' []] `shouldBe` [Node 'a' []]
 
         it "generates forest for two nodes" $ do
-            nestedSetsToForest [NestedSetsNode 1 2 'a' [], NestedSetsNode 3 4 'b' []] `shouldBe` [Node 'a' [], Node 'b' []]
+            nestedSetsToForest [NestedSetsNode (1, 2) 'a' [], NestedSetsNode (3, 4) 'b' []] `shouldBe` [Node 'a' [], Node 'b' []]
 
         it "generates forest for nested nodes" $ do
-            nestedSetsToForest [NestedSetsNode 1 2 'a' [NestedSetsNode 3 4 'b' []]] `shouldBe` [Node 'a' [Node 'b' []]]
+            nestedSetsToForest [NestedSetsNode (1, 2) 'a' [NestedSetsNode (3, 4) 'b' []]] `shouldBe` [Node 'a' [Node 'b' []]]
 
         it "converts nested sets to forest" $ do
             nestedSetsToForest complexNestedSets `shouldBe` complexForest
+
+    describe "nested set position" $ do
+        it "retrieves first position" $ do
+            nestedSetsStartPosition [] `shouldBe` Nothing
+            nestedSetsStartPosition [NestedSetsNode (1, 2) 'a' []] `shouldBe` (Just (1, 2))
+            nestedSetsStartPosition complexNestedSets `shouldBe` (Just (1, 8))
 
 complexForest :: Forest Char
 complexForest = [Node 'a' [
@@ -51,12 +57,12 @@ complexForest = [Node 'a' [
                      Node 'g' []]]
 
 complexNestedSets :: NestedSets Char
-complexNestedSets = [NestedSetsNode 1 8 'a' [
-                         NestedSetsNode 2 7 'b' [
-                             NestedSetsNode 3 4 'c' [],
-                             NestedSetsNode 5 6 'd' []]],
-                     NestedSetsNode 9 14 'e' [
-                         NestedSetsNode 10 11 'f' [],
-                         NestedSetsNode 12 13 'g' []]]
+complexNestedSets = [NestedSetsNode (1, 8) 'a' [
+                         NestedSetsNode (2, 7) 'b' [
+                             NestedSetsNode (3, 4) 'c' [],
+                             NestedSetsNode (5, 6) 'd' []]],
+                     NestedSetsNode (9, 14) 'e' [
+                         NestedSetsNode (10, 11) 'f' [],
+                         NestedSetsNode (12, 13) 'g' []]]
 
 
